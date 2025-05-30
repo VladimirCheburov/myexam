a37add4dbd43f18d79f73e170d4c167196ef4797
VladimirCheburov <wovchanskiy@gmail.com>
2025-05-30 10:57:02 +0300

    Настроена админка для vcexam: поиск, фильтры, M2M, date_hierarchy

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import vcexam
 
-# Register your models here.
+@admin.register(vcexam)
+class vcexamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'exam_date', 'created_at', 'is_public')
+    search_fields = ('name', 'users__email')
+    list_filter = ('is_public', 'created_at', 'exam_date')
+    filter_horizontal = ('users',)
+    date_hierarchy = 'exam_date'
