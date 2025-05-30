a3039261127e1b7251fed59cb45352e5d6b64544
VladimirCheburov <wovchanskiy@gmail.com>
2025-05-30 11:04:39 +0300

    Добавлен редирект с корневого URL на страницу экзаменов

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('vcexam/', views.vcexam_list, name='vcexam_list'),
 ] 
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import vcexam
 
 # Create your views here.
+
+def index(request):
+    return redirect('vcexam_list')
 
 def vcexam_list(request):
     exams = vcexam.objects.filter(is_public=True)
